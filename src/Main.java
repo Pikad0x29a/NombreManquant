3544c047f558fa517fcc855901f16954a56ef3d9
Pikad0x29a <pikad0x29a.root@proton.me>
2025-02-03 15:15:57 +0100

    Mise à Jour du README.md et mise en oeuvre de l'algorithme dans le Main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,59 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.xml.transform.Source;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Entre un nombre N :");
+        int N = scanner.nextInt();
+
+        int[] listeN = new int[N + 1];
+
+        for (int i = 0; i < N + 1; i++) {
+            listeN[i] = i + 1;
         }
+
+        /* Permet de tester si ma liste affiche bien de 1 à N+1
+        System.out.println(Arrays.toString(listeN));*/
+
+        Random nombreAleatoire = new Random();
+        int indexNombreAleatoire = nombreAleatoire.nextInt(listeN.length);
+        int nombreManquant = listeN[indexNombreAleatoire];
+
+        /*Pour vérifier que le nombre manquant a bien été choisi
+        System.out.println("Nombre manquant : " + nombreManquant); */
+
+        int[] listeSansNombreManquant = new int[N];
+        int j = 0;
+        for (int i = 0; i < listeN.length; i++) {
+            if (i != indexNombreAleatoire) {
+                listeSansNombreManquant[j] = listeN[i];
+                j++;
+            }
+        }
+        //Pour vérifier que le nouveau tableau s'affiche sans le nombre manquant:
+        System.out.println(Arrays.toString(listeSansNombreManquant));
+
+
+        int sommeListeN = calculerSomme(listeN);
+        int sommeListeSansNombreManquant = calculerSomme(listeSansNombreManquant);
+        int resultatNombreManquant = sommeListeN - sommeListeSansNombreManquant;
+
+        /*Pour vérifier les sommes:
+        System.out.println("Somme ListeN :" + sommeListeN);
+        System.out.println("Somme liste sans nombre manquant :" +sommeListeSansNombreManquant);*/
+        System.out.println(resultatNombreManquant);
     }
+
+    public static int calculerSomme(int[] tableau) {
+        int somme = 0;
+        for (int i = 0; i < tableau.length; i++) {
+            somme += tableau[i];
+        }
+        return somme;
+    }
+
 }
